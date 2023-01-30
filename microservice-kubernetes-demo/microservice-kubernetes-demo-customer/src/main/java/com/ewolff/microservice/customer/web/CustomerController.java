7777a5a029b181b57cbf73f47442cad65af7016d
juanmera01 <Uo277406@uniovi.es>
2023-01-30 13:03:39 +0100

    upload app files

---
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/web/CustomerController.java
@@ -0,0 +1,73 @@
+package com.ewolff.microservice.customer.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.ewolff.microservice.customer.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ewolff.microservice.customer.Customer;
+
+@Controller
+public class CustomerController {
+
+	private CustomerRepository customerRepo;
+
+	@Autowired
+	public CustomerController( CustomerRepository customerRepo) {
+		this.customerRepo = customerRepo;
+	}
+
+	@RequestMapping(value = "/{id}.html", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public ModelAndView customer(@PathVariable("id") String id) {
+		return new ModelAndView("customer", "customer",
+				customerRepo.findById(id).get());
+	}
+
+	@RequestMapping("/list.html")
+	public ModelAndView customerList() {
+		return new ModelAndView("customerlist", "customers",
+				customerRepo.findAll());
+	}
+
+	@RequestMapping("/checkpost")
+	public ModelAndView checkpost() {
+
+		customerRepo.save(new Customer("c","Eberhard", "Wolff",
+				"eberhard.wolff@gmail.com", "Unter den Linden", "Berlin"));
+
+		return new ModelAndView("customerlist", "customers",
+				customerRepo.findAll());
+	}
+
+	@RequestMapping(value = "/form.html", method = RequestMethod.GET)
+	public ModelAndView add() {
+		return new ModelAndView("customer", "customer", new Customer());
+	}
+
+	@RequestMapping(value = "/form.html", method = RequestMethod.POST)
+	public ModelAndView post(Customer customer, HttpServletRequest httpRequest) {
+		customer = customerRepo.save(customer);
+		return new ModelAndView("success");
+	}
+
+	@RequestMapping(value = "/{id}.html", method = RequestMethod.PUT)
+	public ModelAndView put(@PathVariable("id") String id, Customer customer,
+							HttpServletRequest httpRequest) {
+		customer.setId(id);
+		customerRepo.save(customer);
+		return new ModelAndView("success");
+	}
+
+	@RequestMapping(value = "/{id}.html", method = RequestMethod.DELETE)
+	public ModelAndView delete(@PathVariable("id") String id) {
+		customerRepo.deleteById(id);
+		return new ModelAndView("success");
+	}
+
+}
---
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/test/java/com/ewolff/microservice/customer/CustomerTestApp.java
@@ -0,0 +1,15 @@
+package com.ewolff.microservice.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class CustomerTestApp {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(CustomerTestApp.class);
+	}
+
+}
